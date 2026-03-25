80df6b4f69ec5a442607657bd6ec48a3b37334ad
TypicalBrother <typical@Typical.localdomain>
2026-03-25 11:59:51 +0100

    Ajout des offres

---
+++ src/Models/ConseilsModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+class ConseilsModel extends Model{
+
+     public function __construct(){
+         $this->data=[];
+         $this->dbh= new \PDO("mysql:host=localhost;dbname = stage4all", self::ADMIN, self::PASS);
+         }
+     public function getData(){
+             return $this->data;
+         }
+
+     public function getFromDataBase(){
+        $stmt = $this->dbh->prepare("SELECT * FROM conseils");
+        $stmt->execute();
+        $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $resultats;
+     }
+}
