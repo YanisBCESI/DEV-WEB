0a4c31355312c6ca6dbcb0ccff782e5118db1d65
kamass <maloxococo@gmail.com>
2026-03-26 11:29:04 +0100

    Mise a jour de la configuration et du modele

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -8,5 +8,7 @@ abstract class Model {
 
     CONST ADMIN = "debian-sys-maint";
     CONST PASS = "CBH8Bs2wCvwY4d00";
+    CONST ADMIN = "stageuser";
+    CONST PASS = "MotDePasseFort123!";
     protected $connection = null;
 }
