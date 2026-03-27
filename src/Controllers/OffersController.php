ffa376c1b42d1f37140fb1f7f6caab34b8639ae4
AdamL0645 <adamlecointre45@gmail.com>
2026-03-27 14:15:49 +0100

    Affichage des offres

--- src/Controllers/OffersController.php
+++ src/Controllers/OffersController.php
@@ -10,18 +10,14 @@ class OffersController extends Controller {
         $this->templateEngine = $templateEngine;
     }
 
-    public function getoffers(){
 
+    public function offersPage(){
         // 1. Récupérer les données
         $offres = $this->Offer_model->getAllOffers();
 
         // 2. Envoyer à la vue
         echo $this->templateEngine->render('offres.html.twig', [
             'offres' => $offres
-        ]);
-    }
-
-    public function offersPage(){
-        echo $this->templateEngine->render("offres.html.twig");
+        ]);    
     }
 }
