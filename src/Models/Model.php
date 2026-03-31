df579eb1d29a08fb98f71ced0c6e089f4830769b
YanisBCESI <yanis.barral@viacesi.fr>
2026-03-31 16:12:19 +0200

    zkejrfhzejkhfjkzehf

---
+++ src/Models/Model.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+/**
+ * The Model class is an abstract class that serves as the base class for all models in the application.
+ */
+abstract class Model {
+
+    CONST ADMIN = "debian-sys-maint";
+    CONST PASS = "LYK8WN3Oup7UxkZW";
+    protected $connection = null;
+}
