59439e33740165b69260fd07fb07569d12b02545
kamass <maloxococo@gmail.com>
2026-03-31 13:09:08 +0200

    Corrige la wishlist et ses derniers ajustements

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -7,6 +7,6 @@ namespace App\Models;
 abstract class Model {
 
     CONST ADMIN = "debian-sys-maint";
-    CONST PASS = "LYK8WN3Oup7UxkZW";
+    CONST PASS = "18htLt63mkhK4qVO";
     protected $connection = null;
 }
