98f44b3585bf61f7e637a96f0a3d0e36209849d6
kamass <maloxococo@gmail.com>
2026-04-01 09:35:29 +0200

    Ajoute la gestion des etudiants et la suppression des pilotes

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ use App\Controllers\OffersController;
 use App\Controllers\FileDepotController;
 use App\Controllers\AccountController;
 use App\Controllers\AdminController;
+use App\Controllers\StudentManagementController;
 use App\Controllers\LegalController;
 use App\Controllers\ConseilsController;
 use App\Controllers\WishlistController;
@@ -24,10 +25,17 @@ $twig = new \Twig\Environment($loader, [
 
 $currentStudent = $_SESSION["student"] ?? null;
 $currentAdmin = $_SESSION["admin"] ?? null;
+$currentPilot = $_SESSION["pilot"] ?? null;
+$currentManager = $currentAdmin ?? $currentPilot;
 $twig->addGlobal("current_student", $currentStudent);
 $twig->addGlobal("current_admin", $currentAdmin);
+$twig->addGlobal("current_pilot", $currentPilot);
+$twig->addGlobal("current_manager", $currentManager);
 $twig->addGlobal("is_student_logged_in", isset($currentStudent["id"]));
 $twig->addGlobal("is_admin_logged_in", isset($currentAdmin["id"]));
+$twig->addGlobal("is_pilot_logged_in", isset($currentPilot["id"]));
+$twig->addGlobal("is_management_logged_in", isset($currentManager["id"]));
+$twig->addGlobal("current_management_role", $currentManager["role"] ?? null);
 $twig->addGlobal("wishlist_icon_path", "/assets/images/30571.png");
 
 if(isset($_GET["uri"])){
@@ -97,6 +105,34 @@ switch($uri){
     case 'admin_pilot_create':
         $AdminController = new AdminController($twig);
         $AdminController->createPilotAccount();
+        break;
+    case 'admin_pilot_delete':
+        $AdminController = new AdminController($twig);
+        $AdminController->deletePilotAccount();
+        break;
+    case 'admin_students':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->studentsPage();
+        break;
+    case 'admin_student_create':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->studentCreatePage();
+        break;
+    case 'admin_student_store':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->storeStudent();
+        break;
+    case 'admin_student_edit':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->studentEditPage();
+        break;
+    case 'admin_student_update':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->updateStudent();
+        break;
+    case 'admin_student_delete':
+        $StudentManagementController = new StudentManagementController($twig);
+        $StudentManagementController->deleteStudent();
         break;
     case "conseils":
         $ConseilsController = new ConseilsController($twig);
